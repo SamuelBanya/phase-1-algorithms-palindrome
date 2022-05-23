f5add3659c2f859fc5bc6901e02c94610e9b406e
Samuel Banya <sbanya@fastmail.com>
2022-05-23 15:11:48 -0400

    Adding latest changes to assignment to include algorithm and solution

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // Write a function isPalindrome that will receive one argument, a string.
+  // Your function should return true if the string is a palindrome
+  // (that is, if it reads the same forwards and backwards, like "mom" or "racecar"),
+  // and return false if it is not a palindrome.
+
+  // A new string variable called 'reverseWord' would need to be declared to incorporate the characters of the reversed word
+  // This would need a word.forEach() loop to then loop for each character of the provided 'word' variable
+  // Upon each iteration of the .forEach() loop, the single character would be added to the new string variable, 'reverseWord'
+  // After the loop is complete, an 'if' statement with a condition to check if 'word' === 'reverseWord'
+
+
+  // Test case:
+  // If that is true, then it is a palindrome
+  // Aka:
+  // If (word === reverseWord) {
+  // return true
+  // }
+
+  let reverseWord = "";
+
+  console.log("typeof(word): ", typeof(word));
+
+  for (let i = word.length - 1; i >= 0; i--) {
+    console.log(`${word}[${i}]: ${word[i]}`);
+    reverseWord += word[i];
+    console.log("reverseWord: ", reverseWord);
+  }
+
+  if (word === reverseWord) {
+    console.log(`Palindrome check: Yes, ${word} === ${reverseWord}`);
+    return true;
+  }
+  else {
+    console.log("Palindrome check: No, it is not a palindrome");
+    return false;
+  }
 }
 
-/* 
+/*
   Add your pseudocode here
 */
 
@@ -23,3 +59,8 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+isPalindrome("abba");
+isPalindrome("racecar");
+isPalindrome("a");
+isPalindrome("ab");
